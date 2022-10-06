9d1a1c3d1eacef00b2aa70bfb38b4fbe45d3d2ea
Ali AbuSaleh <alighabusaleh@gmail.com>
2022-10-07 00:31:17 +0200

    add ubuntu command + rizwan fix for back-slash

--- scripts/mysql_generate_data.sh
+++ scripts/mysql_generate_data.sh
@@ -1,13 +1,14 @@
+
 #!/usr/bin/env bash
 # For generating the TPCDS Data
 # This script will be run to generate mysql compatible data
-# 
+#
 # USAGE: ./mysql_generate_data <SCALE>
 # ./mysql_generate_data 1
 
 SCALE=$1
-DATA_DIR="/Users/rizwan/Downloads/ULB/dw/project/tpcds_toolkit/data/${SCALE}"
-TOOLS_DIR=/Users/rizwan/Downloads/ULB/dw/project/tpcds_toolkit/tools
+DATA_DIR="/home/aliabusaleh/Desktop/ulb/tpcds-kit/data/${SCALE}"
+TOOLS_DIR=/home/aliabusaleh/Desktop/ulb/tpcds-kit/tools
 
 mkdir -p $DATA_DIR
 
@@ -15,4 +16,8 @@ pushd $TOOLS_DIR
 ./dsdgen -SCALE $SCALE -DIR $DATA_DIR -VERBOSE Y
 popd
 
-LC_CTYPE=C && sed -i '' -e 's/^|/\N|/' -e 's/||/|\N|/g' -e 's/||/|\N|/g' -e 's/|$/|/' $DATA_DIR/*.dat
+# For Mac OS
+# LC_CTYPE=C && sed -i '' -e 's_^|_\\N|_g' -e 's_||_|\\N|_g' -e 's_||_|\\N|_g' $DATA_DIR/*.dat
+
+# For Linux
+LC_CTYPE=C && sed -i -e 's_^|_\\N|_g' -e 's_||_|\\N|_g' -e 's_||_|\\N|_g' $DATA_DIR/*.dat

632670ae6101927e87619c0e36d81403a0554609
Ali AbuSaleh <alighabusaleh@gmail.com>
2022-10-10 23:36:21 +0200

    add ubuntu summary scripts

--- scripts/mysql_result_summary.sh
+++ scripts/mysql_result_summary.sh
@@ -7,13 +7,18 @@
 # USAGE: ./mysql_result_summary <SCALE>
 # EXAMPLE: ./mysql_result_summary 1
 
-DB_RESULTS_DIR=/Users/rizwan/Downloads/ULB/dw/project/tpcds_toolkit/query_results/${1}
+DB_RESULTS_DIR=/home/aliabusaleh/PycharmProjects/DataWarehouse/query_results/${1}/
 CSV_NAME=query_time_scale_factor_${1}.csv
 SUMMARY_DIR=`pwd`
 
 rm -rf $SUMMARY_DIR/$CSV_NAME
 
 for f in `ls $DB_RESULTS_DIR/*.log`
-    do TIME=`cat $f | awk '{print $1}'`
-    echo "`basename ${f%.*}`,$TIME" >> $SUMMARY_DIR/$CSV_NAME
+    # Mac OS
+    #do TIME=`cat $f | awk '{print $1}'`
+    #echo "`basename ${f%.*}`,$TIME" >> $SUMMARY_DIR/$CSV_NAME
+
+    # ubuntu OS
+    do TIME=`head -1 $f |  awk '{print $3}' | awk '{sub(/\(.*/, "", $(NF-1)); print $1}'`
+    echo "`basename ${f%.*>}`,$TIME" >> $SUMMARY_DIR/$CSV_NAME
 done
